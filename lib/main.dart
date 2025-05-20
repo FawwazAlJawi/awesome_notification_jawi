a4aab05f6b0ca29e5a9bdc3b95441ed14662d17b
FawwazAlJawi <faaljawi04@gmail.com>
2025-05-20 12:29:08 +0700

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:awesome_notification/screens/home_screen.dart';
+import 'package:awesome_notification/screens/second_screen.dart';
+import 'package:awesome_notification/services/notification_service.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NotificationService.initializeNotification();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notification Demo',
+      routes: {
+        'home': (context) => const HomeScreen(),
+        'second': (context) => const SecondScreen(),
+      },
+      initialRoute: 'home',
+      navigatorKey: navigatorKey,
+    );
+  }
+}
